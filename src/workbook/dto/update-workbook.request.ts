063359e1b3b0fe9d6d12b0987f370b019eabdfe8
mokhs00 <mokhs00@naver.com>
2022-01-13 02:28:13 +0900

    update : dto string field @MinLength validation 추가 @IsString()이 적용되어있는 필드에 @MinLength(1) 추가

--- src/workbook/dto/update-workbook.request.ts
+++ src/workbook/dto/update-workbook.request.ts
@@ -1,6 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class UpdateWorkbookRequest {
   @IsString()
+  @MinLength(1, {
+    message: 'title은 공백일 수 없습니다.',
+  })
   title: string;
 }
