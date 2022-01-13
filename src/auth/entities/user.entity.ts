190cf90acbf2f65331c7d7e5b12b8c01682171f4
mokhs00 <mokhs00@naver.com>
2022-01-13 14:02:01 +0900

    add : profile to user.entity

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import * as bcrypt from 'bcrypt';
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Profile } from './profile';
 
 @Entity()
 export class User extends BaseEntity {
@@ -11,6 +12,22 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @Column(() => Profile)
+  profile: Profile;
+
+  static async of(email: string, encodedPassword: string): Promise<User> {
+    const user = new User();
+    user.email = email;
+    user.password = encodedPassword;
+    user.profile = await Profile.of(email, null);
+
+    return user;
+  }
+
+  async updatePassword(encodedPassword: string) {
+    this.password = encodedPassword;
+  }
 
   async validatePassword(rawPassword: string): Promise<boolean> {
     return await bcrypt.compare(rawPassword, this.password);
