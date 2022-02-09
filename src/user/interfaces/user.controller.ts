6d37234c5632ae5a92b10679929bf16bd50e318b
mokhs00 <mokhs00@naver.com>
2022-02-10 06:26:05 +0900

    update : 공통 ApiResponse 적용 in user api

--- src/user/interfaces/user.controller.ts
+++ src/user/interfaces/user.controller.ts
@@ -1,8 +1,10 @@
 import { User } from '@/auth/domain/user.entity';
 import { GetUser } from '@/common/decorators/get-user.decorator';
+import { ApiResponse } from '@/common/response/api.response';
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserService } from '../application/user.service';
+import { UserInfoResponse } from './user-info.response';
 
 @Controller('api/v1/users')
 export class UserController {
@@ -10,8 +12,11 @@ export class UserController {
 
   @UseGuards(AuthGuard())
   @Get('/me')
-  async me(@GetUser() user: User) {
-    const response = await this.userService.me(user);
-    return response;
+  async me(@GetUser() user: User): Promise<ApiResponse<UserInfoResponse>> {
+    const response: UserInfoResponse = await this.userService.me(user);
+    return ApiResponse.of({
+      data: response,
+      message: 'success get my userInfo',
+    });
   }
 }
